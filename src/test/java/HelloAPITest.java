9c32f64743c63272875805ab757b077ad160da6f
Andrew <andreyshalagin@gmail.com>
2023-08-10 09:41:01 +0300

    test: add getHelloResponse test

--- src/test/java/HelloAPITest.java
+++ src/test/java/HelloAPITest.java
@@ -1,8 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import org.junit.jupiter.api.Test;
 
 public class HelloAPITest {
     @Test
     void printHelloWorld(){
         System.out.println("API automation rules!");
+    }
+
+    @Test
+    void getHelloResponse() {
+        String url = "https://playground.learnqa.ru/api/hello";
+        Response response = RestAssured.get(url).andReturn();
+        response.prettyPrint();
     }
 }
