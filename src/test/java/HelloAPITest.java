2cc2847195508df705ebe0a3fe06eae395d7d19e
Andrew <andreyshalagin@gmail.com>
2023-08-16 10:32:37 +0300

    chore: add param/params tests

--- src/test/java/HelloAPITest.java
+++ src/test/java/HelloAPITest.java
@@ -3,6 +3,9 @@ import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloAPITest {
     @Test
     void printHello(){
@@ -14,6 +17,31 @@ public class HelloAPITest {
         String url = "https://playground.learnqa.ru/api/hello";
         Response response = RestAssured.get(url).andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    void testParameter() {
+       String methodURL = "https://playground.learnqa.ru/api/hello";
+        Response response = RestAssured
+                .given()
+                .queryParam("name", "John Snow")
+                .get(methodURL).andReturn();
+        response.prettyPrint();
+    }
+
+    @Test
+    void testParameters() {
+        String methodURL = "https://playground.learnqa.ru/api/hello";
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Sansa Stark");
+
+        Response response = RestAssured
+                .given()
+                .queryParams(params)
+                .get(methodURL)
+                .andReturn();
+        response.prettyPrint();
+
     }
 
     @Test
