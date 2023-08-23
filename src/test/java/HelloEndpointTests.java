67c3a76f5c36e9c6f34d8ccb2027b24e4c2922bc
Andrew <andreyshalagin@gmail.com>
2023-08-23 10:57:21 +0300

    refactor: Переметризация теста через @ValueSource

--- src/test/java/HelloEndpointTests.java
+++ src/test/java/HelloEndpointTests.java
@@ -1,33 +1,34 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloEndpointTests {
     public static final String ENDPOINT_HELLO = "https://playground.learnqa.ru/api/hello";
 
-    @Test
-    void defaultName() {
-        JsonPath response = RestAssured
-                .get(ENDPOINT_HELLO)
-                .jsonPath();
-        String answer = response.getString("answer");
 
-        assertEquals("Hello, someone", answer, "Unexpected response!");
-    }
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John Snow", "Sansa Stark"})
+    void parameterizedName(String name) {
+        Map<String, String> requestParams = new HashMap<>();
+        if (!name.isEmpty()) {
+            requestParams.put("name", name);
+        }
 
-    @Test
-    void parameterizedName() {
-        String name = "John Snow";
         JsonPath response = RestAssured
                 .given()
-                .param("name", name)
+                .params(requestParams)
                 .when()
                 .get(ENDPOINT_HELLO)
                 .jsonPath();
         String answer = response.getString("answer");
+        String expectedName = (name.isEmpty() ? "someone" : name);
 
-        assertEquals(String.format("Hello, %s", name), answer, "Unexpected response!" );
+        assertEquals(String.format("Hello, %s", expectedName), answer, "Unexpected response!");
     }
 }
