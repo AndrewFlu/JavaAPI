18a96db1e33f7aede74d08cbc87faf2e1aaf42c0
Andrew <andreyshalagin@gmail.com>
2023-08-23 10:29:27 +0300

    test: Добавлены тесты на проверку ответа: - без параметра - с параметром

--- src/test/java/HelloEndpointTests.java
+++ src/test/java/HelloEndpointTests.java
@@ -1,54 +1,33 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloEndpointTests {
     public static final String ENDPOINT_HELLO = "https://playground.learnqa.ru/api/hello";
-    @Test
-    void testParameters() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "Sansa Stark");
-
-        Response response = RestAssured
-                .given()
-                .queryParams(params)
-                .get(ENDPOINT_HELLO)
-                .andReturn();
-        response.prettyPrint();
-    }
 
     @Test
-    void testJsonPath() {
+    void defaultName() {
         JsonPath response = RestAssured
-                .given()
-                .queryParam("name", "John Snow")
                 .get(ENDPOINT_HELLO)
                 .jsonPath();
-        String key = "answer";
-        String value = response.get(key);
-        if (value == null) {
-            System.out.printf("The key %s is absent.", key);
-        } else {
-            System.out.println(value);
-        }
+        String answer = response.getString("answer");
+
+        assertEquals("Hello, someone", answer, "Unexpected response!");
     }
 
     @Test
-    void testRestAssured() {
-        Response response = RestAssured.get(ENDPOINT_HELLO).andReturn();
-        response.prettyPrint();
-    }
-
-    @Test
-    void testParameter() {
-        Response response = RestAssured
+    void parameterizedName() {
+        String name = "John Snow";
+        JsonPath response = RestAssured
                 .given()
-                .queryParam("name", "John Snow")
-                .get(ENDPOINT_HELLO).andReturn();
-        response.prettyPrint();
+                .param("name", name)
+                .when()
+                .get(ENDPOINT_HELLO)
+                .jsonPath();
+        String answer = response.getString("answer");
+
+        assertEquals(String.format("Hello, %s", name), answer, "Unexpected response!" );
     }
 }
