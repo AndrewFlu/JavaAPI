eabec77e3cfc012e174d8664d559dc202304e270
Andrew <andreyshalagin@gmail.com>
2023-08-24 13:44:11 +0300

    test: Ex13: User Agent
    - параметризация теста на основе CSV-токена

--- src/test/java/tests/APITests.java
+++ src/test/java/tests/APITests.java
@@ -5,6 +5,8 @@ import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import utils.FileReader;
 
 import java.util.*;
@@ -27,6 +29,7 @@ public class APITests {
     private static final String ENDPOINT_GET_SECRET_PASSWORD = "https://playground.learnqa.ru/ajax/api/get_secret_password_homework";
     private static final String ENDPOINT_HOMEWORK_COOKIE = "https://playground.learnqa.ru/api/homework_cookie";
     private static final String ENDPOINT_HOMEWORK_HEADER = "https://playground.learnqa.ru/api/homework_header";
+    private static final String ENDPOINT_USER_AGENT_CHECK = "https://playground.learnqa.ru/ajax/api/user_agent_check";
 
     @Test
     void getText() {
@@ -344,5 +347,29 @@ public class APITests {
 
         assertEquals("Some secret value", headers.getValue("x-secret-homework-header"),
                 "Unexpected 'x-secret-homework-header' header value");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android"
+            , "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS"
+            , "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, Unknown, Unknown"
+            , "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No"
+            , "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone"
+    })
+    void userAgent(String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice) {
+        Response response = RestAssured
+                .given()
+                .header("user-agent", userAgent)
+                .when()
+                .get(ENDPOINT_USER_AGENT_CHECK)
+                .andReturn();
+        String actualPlatform = response.jsonPath().getString("platform");
+        String actualBrowser = response.jsonPath().getString("browser");
+        String actualDevice = response.jsonPath().getString("device");
+
+        assertEquals(expectedPlatform, actualPlatform, "Unexpected platform");
+        assertEquals(expectedBrowser, actualBrowser, "Unexpected browser");
+        assertEquals(expectedDevice, actualDevice, "Unexpected device");
     }
 }
