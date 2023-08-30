c77aa8ce74293fb942a8808c4cb9596bdb1fea2b
Andrew <andreyshalagin@gmail.com>
2023-08-30 16:59:42 +0300

    chore: Запросы выделены в отдельный класс CoreRequests
    - тесты UserAuthTests переведены на использование запросов из CoreRequests

--- src/test/java/tests/UserAuthTests.java
+++ src/test/java/tests/UserAuthTests.java
@@ -1,12 +1,16 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import lib.Assertions;
 import lib.BaseTestCase;
-import org.junit.jupiter.api.DisplayName;
+import lib.CoreRequests;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,18 +18,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Description;
-
 @Epic("Authorization cases")
 @Feature("Authorisation")
 public class UserAuthTests extends BaseTestCase {
+    private final CoreRequests coreRequests = new CoreRequests();
+
     private static final String ENDPOINT_LOGIN = "https://playground.learnqa.ru/api/user/login";
     private static final String ENDPOINT_AUTH = "https://playground.learnqa.ru/api/user/auth";
-
-    private final String login = "vinkotov@example.com";
-    private final String password = "1234";
 
     private String cookie;
     private String header;
@@ -36,15 +35,13 @@ public class UserAuthTests extends BaseTestCase {
     public void login() {
 
         Map<String, String> authData = new HashMap<>();
+        String login = "vinkotov@example.com";
         authData.put("email", login);
+        String password = "1234";
         authData.put("password", password);
 
-        Response loginEndpointResponse = RestAssured
-                .given()
-                .body(authData)
-                .when()
-                .post(ENDPOINT_LOGIN)
-                .andReturn();
+        Response loginEndpointResponse = coreRequests.makePostRequest(ENDPOINT_LOGIN, authData);
+
         this.cookie = this.getCookie(loginEndpointResponse, "auth_sid");
         this.header = this.getHeader(loginEndpointResponse, "x-csrf-token");
         this.loginUserId = this.getIntFromJson(loginEndpointResponse, "user_id");
@@ -55,13 +52,7 @@ public class UserAuthTests extends BaseTestCase {
     @Description("Check that user can be successfully authenticated by email and password")
     @DisplayName("Test positive auth user")
     void auth() {
-        Response authEndpointResponse = RestAssured
-                .given()
-                .header("x-csrf-token", this.header)
-                .cookie("auth_sid", this.cookie)
-                .when()
-                .get(ENDPOINT_AUTH)
-                .andReturn();
+        Response authEndpointResponse = coreRequests.makeGetRequest(ENDPOINT_AUTH, this.header, this.cookie);
 
         Assertions.assertJsonByName(authEndpointResponse, "user_id", this.loginUserId);
     }
@@ -74,14 +65,13 @@ public class UserAuthTests extends BaseTestCase {
         RequestSpecification spec = RestAssured.given();
         spec.baseUri(ENDPOINT_AUTH);
         if (condition.equals("cookie")) {
-            spec.cookie("auth_sid", this.cookie);
+            Response responseToCheck = coreRequests.makeGetRequestWithCookie(ENDPOINT_AUTH, this.cookie);
+            Assertions.assertJsonByName(responseToCheck, "user_id", 0);
         } else if (condition.equals("headers")) {
-            spec.header("x-csrf-token", this.header);
+            Response responseToCheck = coreRequests.makeGetRequestWithToken(ENDPOINT_AUTH, this.header);
+            Assertions.assertJsonByName(responseToCheck, "user_id", 0);
         } else {
             throw new IllegalArgumentException("Unrecognized test parameter: " + condition);
         }
-        Response authResponse = spec.get().andReturn();
-
-        Assertions.assertJsonByName(authResponse, "user_id", 0);
     }
 }
